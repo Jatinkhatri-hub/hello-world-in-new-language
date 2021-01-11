d865a435db49db90833788a52bcb9861d3bd8352
Jatinkhatri-hub <jatinkhatri245@gmail.com>
2021-01-11 22:02:59 +0530

    Created console app

---
+++ Myapp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var name = "Jatin Khatri";
+
+            var location = "I'm from India";
+
+            var myCollege = "Lachoo Memorial College Of Science And Technology (AUTONOMOUS), Jodhpur";
+
+            Console.WriteLine("I'm " + name);
+
+            Console.WriteLine(location);
+
+            Console.WriteLine("I'm from " + myCollege);
+            
+
+
+        }
+    }
+}
